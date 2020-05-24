84864c74afcc63da5d907feebbbbb4b699f44cde
jlrussin <jlrussin@gmail.com>
2020-05-24 12:12:52 -0700

    Added scripts

---
+++ scripts/defaults_simple.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#SBATCH -p localLimited
+#SBATCH -A ecortex
+#SBATCH --mem=1G
+#SBATCH --time=12:00:00
+#SBATCH -c 2
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+gpus=$(echo $CUDA_VISIBLE_DEVICES | tr "," "\n")
+for gpu in $gpus
+do
+echo "Setting fan for" $gpu "to full"
+nvidia_fancontrol full $gpu
+done
+
+python main.py \
+--split simple \
+--batch_size 32 \
+--num_epochs 10 \
+--model_type transformer \
+--d_model 12 \
+--nhead 2 \
+--num_encoder_layers 2 \
+--num_decoder_layers 2 \
+--dim_feedforward 20 \
+--dropout 0.1 \
+--learning_rate 0.001 \
+--results_dir ../results/ \
+--out_data_file train_defaults_simple.json \
+--checkpoint_path ../weights/defaults_simple.pt \
+--checkpoint_every 5 \
+--record_loss_every 20
+
+for gpu in $gpus
+do
+echo "Setting fan for " $gpu "back to auto"
+nvidia_fancontrol auto $gpu
+done
